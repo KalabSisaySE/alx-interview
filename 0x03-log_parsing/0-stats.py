9790fda48eb1b059e30a6fdbd8ed659e1322ec32
KalabSisaySE <kyimusic11@gmail.com>
2023-01-20 20:42:12 +0300

    test for the checker three

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,10 +2,5 @@
 """the `0-stats` module
 reads logs from stdin and parses it to compute some metrics
 """
-from dateutil import parser
-from functools import reduce
-import re
-import sys
 
-
-# comment
+# empty module

e686c36e848c83b67a3d74ddb940bdb52978f1a3
KalabSisaySE <kyimusic11@gmail.com>
2023-01-23 13:28:19 +0300

    fix: task-0 4 imports

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,7 @@
 """the `0-stats` module
 reads logs from stdin and parses it to compute some metrics
 """
-import sys
+from datetime import datetime
 from functools import reduce
 import re
+import sys

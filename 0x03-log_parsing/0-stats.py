af7d8d3a421459a8c13199b0faaa18c02d4c6338
KalabSisaySE <kyimusic11@gmail.com>
2023-01-23 15:03:27 +0300

    fix task-0 remove unused import

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,7 +5,6 @@ reads logs from stdin and parses it to compute some metrics
 from datetime import datetime
 from functools import reduce
 import re
-import select
 import sys
 
 
@@ -20,17 +19,23 @@ def date_valid(date):
 
 def print_metrics():
     """analyzes `logs` and prints the metrics"""
-    file_sizes = list(map(lambda x: x[3], logs))
-    file_size = reduce(lambda x, y: x + y, file_sizes)
-    all_status_codes = list(map(lambda x: x[2], logs))
-    # gets the uniques status codes from list and sorts it
-    # since set() only stores unique values
-    unique_status_codes = list(set(all_status_codes))
-    unique_status_codes.sort()
+    # redirected from file or pipe
+    if not sys.stdin.isatty():
+        file_sizes = list(map(lambda x: x[3], logs))
+        file_size = reduce(lambda x, y: x + y, file_sizes)
+        all_status_codes = list(map(lambda x: x[2], logs))
+        # gets the uniques status codes from list and sorts it
+        # since set() only stores unique values
+        unique_status_codes = list(set(all_status_codes))
+        unique_status_codes.sort()
 
-    print("File size: {}".format(file_size))
-    for stat_code in unique_status_codes:
-        print("{}: {}".format(stat_code, all_status_codes.count(stat_code)))
+        print("File size: {}".format(file_size))
+        for stat_code in unique_status_codes:
+            print("{}: {}".format(
+                stat_code,
+                all_status_codes.count(stat_code))
+            )
+
 
 # if redirected from file or pipe
 if not sys.stdin.isatty():

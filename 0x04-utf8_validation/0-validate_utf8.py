0d06b5a3fb7bb373ae077be301be557725404a2e
KalabSisaySE <kyimusic11@gmail.com>
2023-01-25 16:40:16 +0300

    fix task-0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,13 @@ defines the function `validUTF8`
 
 def validUTF8(data):
     """determines if a given data set represents a valid UTF-8 encoding"""
+    encoding = []
     for char in data:
-        if len(bin(char).replace('0b', '')) > 8:
-            return False
-    return True
+        # trim 0b from the string
+        byte = bin(char).replace("0b", "")
+        if len(byte) > 8:
+            # 8 LSB
+            byte = byte[-8:]
+        encoding.append(byte)
+    encoding_str = "-".join(encoding)
+    return "00000000" in encoding_str

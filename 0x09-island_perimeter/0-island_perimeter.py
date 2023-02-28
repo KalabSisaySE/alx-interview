0657b579c302f1c554d2f1d0135773a55717c2b9
KalabSisaySE <kyimusic11@gmail.com>
2023-02-28 12:18:02 +0300

    fix task-0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,15 +7,18 @@ defines the function `island_perimeter`
 def island_perimeter(grid):
     """returns the perimeter of the island, if it exists in the grid"""
     # finding the first '1' or the edge of the island
-    for row in grid:
-        try:
-            row.index(1)
-            start_row = grid.index(row)
-            break  # as soon as the first one is found
-        except ValueError:
-            continue
+    if grid and type(grid) is list and type(grid[0]) is list:
+        start_row = -1
+        for row in grid:
+            try:
+                row.index(1)
+                start_row = grid.index(row)
+                break  # as soon as the first one is found
+            except ValueError:
+                continue
+        if start_row == -1:
+            return 0  # there exists no island
 
-    if start_row:
         perimeter = 0
         for index, row in enumerate(grid[start_row:]):
             # if row does not contain any zeros
@@ -44,4 +47,5 @@ def island_perimeter(grid):
                         if grid[current_row][current_col + 1] == 0:
                             perimeter = perimeter + 1
         return perimeter
+
     return 0

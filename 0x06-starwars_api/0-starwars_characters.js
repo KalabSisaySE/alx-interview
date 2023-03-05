5d43402ec5d3fce28204b1f3d371a2c526731eea
KalabSisaySE <kyimusic11@gmail.com>
2023-03-05 12:04:24 +0300

    fix task-0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,33 +2,34 @@
 const request = require('request');
 
 if (process.argv[2]) {
-  const options = {
-    url: `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`,
-    method: 'GET'
-  };
+  function asyncGetData (url) {
+    return new Promise((resolve, reject) => {
+      request.get(url, function (err, res, body) {
+        if (err) {
+          resolve(err);
+        } else if (res.statusCode !== 200) {
+          reject(new Error(`status code: ${res.statusCode}`));
+        } else {
+          const data = JSON.parse(body);
+          resolve(data);
+        }
+      });
+    });
+  }
 
-  request(options, function (err, res, body) {
-    if (err) {
-      console.log(err);
-    } else if (res.statusCode === 200) {
-      const characters = JSON.parse(res.body).characters;
+  async function getData () {
+    try {
+      const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+      const data = await asyncGetData(url);
+      const characters = data.characters;
       for (const character of characters) {
-        // console.log(character.slice(43, character.length));
-        const opt = {
-          url: character,
-          method: 'GET'
-        };
-        request(opt, function (err, res, body) {
-          if (err) {
-            console.log(err);
-          } else if (res.statusCode === 200) {
-            console.log(JSON.parse(body).name);
-          }
-        });
+        const characterData = await asyncGetData(character);
+        console.log(characterData.name);
       }
+    } catch (err) {
+      console.log(`err: ${err}`);
     }
-  });
-//   let url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
-//   url = `https://swapi-api.alx-tools.com/api/films/`;
-//   console.log(url.slice(42, 43))
+  }
+
+  getData();
 }
